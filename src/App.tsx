8da07d257a291e79d545730c25438aadf27daef2
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-12 21:56:30 +0900

    axiosで型指定した変数を取得 取得した変数は useState のフックスで設定した userProfiles で読み込んであげる。

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./styles.css";
 
 import { UserCard } from "./components/UserCard";
 import { UserProfile } from "./types/userProfile";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import axios from "axios";
 
 const user: UserProfile = {
   id: 10,
@@ -11,8 +14,29 @@ const user: UserProfile = {
 };
 
 export default function App() {
+  // 取得したデータを state　として保持する
+  // userProfiles は axios からとってきたデータの中からいくつかを抜き出して利用するイメージ
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  // クリックイベントとして定義
+  const onClickFetchUser = () => {
+    // 取得するデータの型をあらかじめ設定する
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      // 取得したデータの中から必要なデータを抽出して state の変数に設定
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
+  };
   return (
     <div className="App">
+      <button onClick={onClickFetchUser}> data acquisition </button>
+      {console.log(userProfiles)}
       <UserCard user={user} />
     </div>
   );
