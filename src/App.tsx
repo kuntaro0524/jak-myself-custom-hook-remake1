7107581b404dff58a4c36d7030c588aa391281cd
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-13 10:02:28 +0900

    いろんな状態管理を実装した useStateを利用して、読み込み中であるかどうか、エラーが出たかどうか、ということを管理するフラグを準備して、それを利用してレンダリングの出し分けができるようにした。

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,18 @@ import { User } from "./types/api/user";
 import { useState } from "react";
 import axios from "axios";
 
-const user: UserProfile = {
-  id: 10,
-  name: "Kunio Hirata",
-  email: "kuntaro@gmail",
-  address: "Kouto 2-10-36"
-};
-
 export default function App() {
   // 取得したデータを state　として保持する
   // userProfiles は axios からとってきたデータの中からいくつかを抜き出して利用するイメージ
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  // データをロードしているときにはloadingと表示するためのステート管理
+  const [loading, setLoading] = useState(false);
+  // errorの判定をするためのステート管理
+  const [error, setError] = useState(false);
+
   // クリックイベントとして定義
   const onClickFetchUser = () => {
+    setLoading(true);
     // 取得するデータの型をあらかじめ設定する
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
@@ -31,15 +30,29 @@ export default function App() {
           address: `${user.address.city}${user.address.suite}${user.address.street}`
         }));
         setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}> data acquisition </button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
-      <UserCard user={user} />
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}> Error 出てるねん </p>
+      ) : loading ? (
+        <p> Loading </p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
